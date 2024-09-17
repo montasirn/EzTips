082445cd70cd2bfefa2613849634f7ba4b19fbeb
BT_2S14_17 <montasirn@>
2024-09-17 10:10:13 -0400

    Initial commit, with main draft of the script the program has all functionality

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        //Setting up decimal format
+        DecimalFormat df1 = new DecimalFormat("#.00");
+        //User input for amounts
+        System.out.println("Welcome to EzTip, enter the amounts as required without symbols!");
+        System.out.print("Enter your total: ");
+        String totalString = s.nextLine();
+        System.out.print("What percent you want to tip: ");
+        String tipString = s.nextLine();
+        System.out.print("How many people are splitting: ");
+        String numPeopleString = s.nextLine();
+        //converting variables
+        double total = Double.parseDouble(totalString);
+        double tip = Double.parseDouble(tipString);
+        int numPeople = Integer.parseInt(numPeopleString);
+        //performing calculations
+        double totalTipAmount = (tip / 100) * total;
+        double totalBillCost = totalTipAmount + total;
+        double tipPerPerson = totalTipAmount / numPeople;
+        double totalPerPerson = totalBillCost / numPeople;
+        //Printing result
+        System.out.println("Total Tip Amount: $" + df1.format(totalTipAmount) );
+        System.out.println("Total Bill Cost: $" + df1.format(totalBillCost));
+        System.out.println("Tip Per Person: $" + df1.format(tipPerPerson));
+        System.out.println("Total Bill Per Person: $" + df1.format(totalPerPerson));
+
+    }
+}
