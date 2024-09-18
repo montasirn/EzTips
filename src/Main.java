106028ba7e3364ddb39873e7a47262c4d4720909
BT_2S14_17 <montasirn@>
2024-09-18 09:57:34 -0400

    Indentations as well as more descriptive comments, better readability overall

--- src/Main.java
+++ src/Main.java
@@ -6,51 +6,52 @@ public class Main {
 
         Scanner s = new Scanner(System.in);
 
-        //Setting up decimal format
+        //Setting up decimal format for currency, decimals is 2 decimal places
 
-        DecimalFormat df1 = new DecimalFormat("#.00");
+            DecimalFormat df1 = new DecimalFormat("#.00");
 
-        //User input for amounts
+        //User input for total, tip percent and number of people
 
-        System.out.println("Welcome to EzTip, enter the amounts as required without symbols!");
+            System.out.println("Welcome to EzTip, enter the amounts as required without symbols!");
 
-        System.out.print("Enter your total ($): ");
-        String totalString = s.nextLine();
+            System.out.print("Enter your total ($): ");
+            String totalString = s.nextLine();
 
-        System.out.print("What percent you want to tip: ");
-        String tipString = s.nextLine();
+            System.out.print("What percent you want to tip: ");
+            String tipString = s.nextLine();
 
-        System.out.print("How many people are splitting: ");
-        String numPeopleString = s.nextLine();
+            System.out.print("How many people are splitting: ");
+            String numPeopleString = s.nextLine();
 
-        //converting variables
+        //converting variables from strings to most reasonable classes
 
-        double total = Double.parseDouble(totalString);
+            double total = Double.parseDouble(totalString);
 
-        double tip = Double.parseDouble(tipString);
+            double tip = Double.parseDouble(tipString);
 
-        int numPeople = Integer.parseInt(numPeopleString);
+            int numPeople = Integer.parseInt(numPeopleString);
 
         //performing calculations
 
-        double totalTipAmount = (tip / 100) * total;
+            double totalTipAmount = (tip / 100) * total;
 
-        double totalBillCost = totalTipAmount + total;
+            double totalBillCost = totalTipAmount + total;
 
-        double tipPerPerson = totalTipAmount / numPeople;
+            double tipPerPerson = totalTipAmount / numPeople;
 
-        double totalPerPerson = totalBillCost / numPeople;
+            double totalPerPerson = totalBillCost / numPeople;
 
         //Printing result
-        System.out.println();
 
-        System.out.println("Total Tip Amount: $" + df1.format(totalTipAmount) );
+            System.out.println();
 
-        System.out.println("Total Bill Cost: $" + df1.format(totalBillCost));
+            System.out.println("Total Tip Amount: $" + df1.format(totalTipAmount) );
 
-        System.out.println("Tip Per Person: $" + df1.format(tipPerPerson));
+            System.out.println("Total Bill Cost: $" + df1.format(totalBillCost));
 
-        System.out.println("Total Bill Per Person: $" + df1.format(totalPerPerson));
+            System.out.println("Tip Per Person: $" + df1.format(tipPerPerson));
+
+            System.out.println("Total Bill Per Person: $" + df1.format(totalPerPerson));
 
         //close scanner
 
